64f444fb4b28d52b34dffa3368b4133e11509580
kentaro <k.moco0109@gmail.com>
2024-02-13 01:09:32 +0900

    アプリ作成追加

---
+++ trip_liq/trip_liq/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  trip_liq
+//
+//  Created by 健太郎 on 2024/02/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ trip_liq/trip_liq/trip_liqApp.swift
@@ -0,0 +1,17 @@
+//
+//  trip_liqApp.swift
+//  trip_liq
+//
+//  Created by 健太郎 on 2024/02/13.
+//
+
+import SwiftUI
+
+@main
+struct trip_liqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
