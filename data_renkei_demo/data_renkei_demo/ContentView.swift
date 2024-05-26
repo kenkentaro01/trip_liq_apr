d5198b6a9cf9ce9c61a3e7bdc63db3fd01c2dce0
kentaro <k.moco0109@gmail.com>
2024-05-26 23:56:31 +0900

    data_renkei_demoの追加

---
+++ data_renkei_demo/data_renkei_demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  data_renkei_demo
+//
+//  Created by 健太郎 on 2024/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ data_renkei_demo/data_renkei_demo/data_renkei_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  data_renkei_demoApp.swift
+//  data_renkei_demo
+//
+//  Created by 健太郎 on 2024/05/26.
+//
+
+import SwiftUI
+
+@main
+struct data_renkei_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
